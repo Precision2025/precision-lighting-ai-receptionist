a84cc3cd26202478a812f52d2b5c416e46d9c166
Precision2025 <travis@ThePrecisionLighting.com>
2026-08-07 16:56:47 -0500

    Joshua_Field_Phase28_62_V4_ServiceChannel_API_SAFE

---
+++ public/joshua-field-sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'joshua-field-v1';
+const SHELL = ['/field', '/field/', '/field/manifest.json'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  if (new URL(event.request.url).pathname.startsWith('/api/')) return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then(r => r || caches.match('/field'))));
+});
